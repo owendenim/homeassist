3aa34daea3650a4caba8ce9271a306a82650af51
zerzhang <916250497@qq.com>
2025-05-20 17:19:40 +0800

    add support for strip light3 and floor lamp

--- homeassistant/components/switchbot/__init__.py
+++ homeassistant/components/switchbot/__init__.py
@@ -92,6 +92,8 @@ PLATFORMS_BY_TYPE = {
     ],
     SupportedModels.AIR_PURIFIER.value: [Platform.FAN, Platform.SENSOR],
     SupportedModels.AIR_PURIFIER_TABLE.value: [Platform.FAN, Platform.SENSOR],
+    SupportedModels.FLOOR_LAMP.value: [Platform.LIGHT, Platform.SENSOR],
+    SupportedModels.STRIP_LIGHT_3.value: [Platform.LIGHT, Platform.SENSOR],
 }
 CLASS_BY_DEVICE = {
     SupportedModels.CEILING_LIGHT.value: switchbot.SwitchbotCeilingLight,
@@ -117,6 +119,8 @@ CLASS_BY_DEVICE = {
     SupportedModels.LOCK_ULTRA.value: switchbot.SwitchbotLock,
     SupportedModels.AIR_PURIFIER.value: switchbot.SwitchbotAirPurifier,
     SupportedModels.AIR_PURIFIER_TABLE.value: switchbot.SwitchbotAirPurifier,
+    SupportedModels.FLOOR_LAMP.value: switchbot.SwitchbotStripLight3,
+    SupportedModels.STRIP_LIGHT_3.value: switchbot.SwitchbotStripLight3,
 }
 
 
